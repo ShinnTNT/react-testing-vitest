5b551718f8bb3a8e09d36e2e4e5bb9df81491f95
shinnthant2004 <shinthantequi@gmail.com>
2024-04-22 11:18:37 +0630

    feat: first component testing

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "vitest";
+
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("It should render Hello with the name when name is provided.", () => {
+    render(<Greet name="shinnthant" />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/shinnthant/i);
+  });
+
+  it("It should render login button  when name is not provided.", () => {
+    render(<Greet name="" />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
